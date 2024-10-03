d45f0af602e247e9cd743ea8e87c33bd846448b9
Rohan120805 <rohand1208@gmail.com>
2024-10-03 10:05:18 +0530

    rohan

--- App1/settings.py
+++ App1/settings.py
@@ -29,8 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['appointmentguru.onrender.com','127.0.0.1']
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- App1/urls.py
+++ App1/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('AppointmentGuru.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
